4bd7d5f23abdca0f5e5361b61d8860b82fb172de
Hobson Lane <hobs@totalgood.com>
2021-10-23 16:31:12 -0500

    conda_install fixed

--- scripts/conda_install.sh
+++ scripts/conda_install.sh
@@ -1,9 +1,9 @@
-conda create -n nlpia2 'python==3.8.8'
-conda activate nlpia2
-conda install jupyter spacy seaborn scipy scikit-learn transformers regex pytest
-# conda install nltk
-conda install -c pytorch pytorch torchtext
-python spacy download en_core_web_sm
-python spacy download en_core_web_md
+conda activate nlpia2 || conda create -n nlpia2 'python==3.8.8' && conda activate nlpia2
+conda install -c pytorch -y jupyter spacy seaborn scipy scikit-learn transformers regex pytest lxml nltk pytorch torchtext
+python ./conda_install_spacy_en_core_web_md.py || python ./scripts/conda_install_spacy_en_core_web_md.py
+
+# python -c "import spacy; import os; from pathlib import Path; nlp=spacy.load('en_core_web_sm'); modeldir=Path(nlp._path).parent.parent; files = os.listdir(modeldir); assert(any(f.startswith('en_core_web_sm') for f in files))" || python -m spacy download en_core_web_sm
+# python -c "import spacy; import os; from pathlib import Path; nlp=spacy.load('en_core_web_sm'); modeldir=Path(nlp._path).parent.parent; files = os.listdir(modeldir); assert(any(f.startswith('en_core_web_md') for f in files))" || python -m spacy download en_core_web_md
+
 
 
