9f18107720b0bcd2823c90d4cdad3bd2919b5c09
Hobson Lane <hobs@totalgood.com>
2021-10-29 23:14:37 -0500

    abridged name model works well without case-folding

--- src/nlpia2/ch05/mob_programming_kemal_kazuma_labeled_female_abridged.md.py
+++ src/nlpia2/ch05/mob_programming_kemal_kazuma_labeled_female_abridged.md.py
@@ -24,59 +24,95 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression  # noqa
-"""
->>> df = pd.read_csv('.nlpia2-data/baby-names-region.csv.gz')
->>> vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(1, 3))
->>> vectorizer.fit(tqdm(df['name'][istrain]))
->>> vecs = vectorizer.transform(tqdm(df['name']))
->>> vecs
-"""
 
-"""
->>> np.random.seed(451)
->>> istrain = np.random.rand(len(df)) < .
->>> istrain.sum() / len(istrain)
+
 """
 >>> from constants import DATA_DIR
 >>> DATA_DIR
-....nlpia-data/
+PosixPath('.../.nlpia-data/')
 """
 from constants import DATA_DIR
 
+
+"""
+>>> np.random.seed(451)
+>>> df = pd.read_csv(DATA_DIR / 'baby-names-region.csv.gz')
+>>> df.sample(3).T
+         6139665   2565339   22297
+region        WV        MD        AK
+sex            F         F         M
+year        1987      1954      1988
+name    Brittani       Ida   Maxwell
+count         10        18         5
+freq    0.000003  0.000005  0.000001
+"""
 df = pd.read_csv(DATA_DIR / 'baby-names-region.csv.gz')
+df.sample(3).T
+#          6139665   2565339   22297
+# region        WV        MD        AK
+# sex            F         F         M
+# year        1987      1954      1988
+# name    Brittani       Ida   Maxwell
+# count         10        18         5
+# freq    0.000003  0.000005  0.000001
+
+
+"""
+>>> istrain = np.random.rand(len(df))
+>>> istrain.sum() / len(istrain)
+0.8997952854283825
+"""
+np.random.seed(451)
+istrain = np.random.rand(len(df)) < .9
+istrain.sum() / len(istrain)
+# 0.8997952854283825
+
+"""
+>>> vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(1, 3))
+>>> vectorizer.fit(tqdm(df['name'][istrain]))
+>>> feature_names = list(vectorizer.get_feature_names_out())
+>>> len(feature_names)
+5964
+>>> feature_names[:5]
+['a', 'aa', 'aab', 'aac', 'aad']
+"""
 vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(1, 3))
 vectorizer.fit(tqdm(df['name'][istrain]))
+feature_names = list(vectorizer.get_feature_names_out())
+len(feature_names)
+# 5964
+feature_names[:5]
+# ['a', 'aa', 'aab', 'aac', 'aad']
+
+
+"""
+>>> vecs = vectorizer.transform(tqdm(df['name']))
+>>> vecs
+<6241373x5964 sparse matrix of type '<class 'numpy.float64'>'
+    with 86718620 stored elements in Compressed Sparse Row format>
+"""
 vecs = vectorizer.transform(tqdm(df['name']))
+vecs
+# <6241373x5964 sparse matrix of type '<class 'numpy.float64'>'
+#     with 86718620 stored elements in Compressed Sparse Row format>
+
 
 """
->>> istrain = np.random.rand(len(df)) < .9
-
-"""
-"""
->>> model = LogisticRegression(max_iter=10000)
+>>> model = LogisticRegression(C=1, max_iter=2000)
 >>> model.fit(vecs[istrain], df['sex'][istrain], sample_weight=df['freq'][istrain])
-LogisticRegression(max_iter=10000)
+LogisticRegression(C=1, max_iter=2000)
 """
+model = LogisticRegression(C=1, max_iter=2000)
+model.fit(vecs[istrain], df['sex'][istrain], sample_weight=df['freq'][istrain])
+# LogisticRegression(max_iter=2000)
+
 
 """
 >>> names = [
-...    'Maria', 'Aditi', 'Jessica', 'Olessya', 'Una', 'Hanna', 'Winnie', 'Olessya',
-...    'Sylvia', 'Vish', 'Mohammed', 'Jon', 'John', 'Ted', 'Kazuma', 'Meijke', 'Kemal']
-"""
-
-model = LogisticRegression(alpha=1, max_iter=10000)
-model.fit(vecs[istrain], df['sex'][istrain], sample_weight=df['freq'][istrain])
-# LogisticRegression(max_iter=10000)
-
-
-names = [
-    'Maria', 'Aditi', 'Jessica', 'Olessya', 'Una', 'Hanna', 'Winnie', 'Olessya',
-    'Sylvia', 'Vish', 'Mohammed', 'Jon', 'John', 'Ted', 'Kazuma', 'Meijke', 'Kemal']
-
-"""
-names = [
-    'Maria', 'Aditi', 'Jessica', 'Olessya', 'Una', 'Hanna', 'Winnie', 'Olessya',
-    'Sylvia', 'Vish', 'Mohammed', 'Jon', 'John', 'Ted', 'Kazuma', 'Meijke', 'Kemal']
+...     'Maria', 'Aditi', 'Jessica', 'Olessya', 'Una',
+...     'Hanna', 'Winnie', 'Olessya', 'Sylvia',
+...     'Vish', 'Mohammed', 'Jon', 'John', 'Ted',
+...     'Meijke', 'Uzo', 'Kazuma', 'Kemal']
 >>> pd.Series(model.predict(vectorizer.transform(names)), index=names)
 Maria       F
 Aditi       F
@@ -92,113 +128,255 @@ Mohammed    M
 Jon         M
 John        M
 Ted         M
-Kazuma      F
 Meijke      M
+Uzo         M
+Kazuma      F
 Kemal       F
-dtype: object
+"""
+names = [
+    'Maria', 'Aditi', 'Jessica', 'Olessya', 'Una',
+    'Hanna', 'Winnie', 'Olessya', 'Sylvia',
+    'Vish', 'Mohammed', 'Jon', 'John', 'Ted',
+    'Meijke', 'Uzo', 'Kazuma', 'Kemal']
+print(pd.Series(model.predict(vectorizer.transform(names)),
+                index=names))
+# Maria       F
+# Aditi       F
+# Jessica     F
+# Olessya     F
+# Una         F
+# Hanna       F
+# Winnie      F
+# Olessya     F
+# Sylvia      F
+# Vish        M
+# Mohammed    M
+# Jon         M
+# John        M
+# Ted         M
+# Meijke      M
+# Uzo         M
+# Kazuma      F
+# Kemal       F
+# dtype: object
+
+
+"""
+>>> vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(1, 3),
+...                              lowercase=False)
+>>> vectorizer.fit(tqdm(df['name'][istrain]))
+TfidfVectorizer(analyzer='char', lowercase=False, ngram_range=(1, 3))
+"""
+vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(1, 3),
+                             lowercase=False)
+print(vectorizer.fit(tqdm(df['name'][istrain])))
+# TfidfVectorizer(analyzer='char', lowercase=False, ngram_range=(1, 3))
+
+
+"""
+>>> feature_names = list(vectorizer.get_feature_names_out())
+>>> len(feature_names)
+8624
+>>> feature_names[:5]
+['A', 'Aa', 'Aab', 'Aad', 'Aah']
+"""
+feature_names = list(vectorizer.get_feature_names_out())
+print(len(feature_names))
+# 8624
+print(feature_names[:5])
+# ['A', 'Aa', 'Aab', 'Aad', 'Aah']
+
+
+"""
+>>> vecs = vectorizer.transform(tqdm(df['name']))
+>>> vecs
+<6241373x8624 sparse matrix of type '<class 'numpy.float64'>'
+    with 87648303 stored elements in Compressed Sparse Row format>
+"""
+vecs = vectorizer.transform(tqdm(df['name']))
+vecs
+# <6241373x8624 sparse matrix of type '<class 'numpy.float64'>'
+#     with 87648303 stored elements in Compressed Sparse Row format>
+
+
+"""
+>>> model = LogisticRegression(C=1, max_iter=2000)
+>>> model.fit(vecs[istrain], df['sex'][istrain],
+...           sample_weight=df['freq'][istrain])
+LogisticRegression(C=1, max_iter=2000)
+"""
+model = LogisticRegression(C=1, max_iter=2000)
+model.fit(vecs[istrain], df['sex'][istrain],
+          sample_weight=df['freq'][istrain])
+# LogisticRegression(C=1, max_iter=2000)
+
+
+"""
+>>> names = [
+...     'Maria', 'Aditi', 'Jessica', 'Una',
+...     'Hanna', 'Winnie', 'Olessya', 'Sylvia',
+...     'Vish', 'Mohammed', 'Jon', 'John', 'Ted',
+...     'Meijke', 'Uzo', 'Kazuma', 'Kemal']
+>>> pd.Series(model.predict(vectorizer.transform(names)), index=names)
+Maria       F
+Aditi       F
+Jessica     F
+Una         F
+Hanna       F
+Winnie      F
+Olessya     M
+Sylvia      F
+Vish        M
+Mohammed    M
+Jon         M
+John        M
+Ted         M
+Meijke      M
+Uzo         M
+Kazuma      F
+Kemal       M
+"""
+names = [
+    'Maria', 'Aditi', 'Jessica', 'Una',
+    'Hanna', 'Winnie', 'Olessya', 'Sylvia',
+    'Vish', 'Mohammed', 'Jon', 'John', 'Ted',
+    'Meijke', 'Uzo', 'Kazuma', 'Kemal']
+print(pd.Series(model.predict(vectorizer.transform(names)),
+                index=names))
+
+
+"""
 >>> (df['name'] == 'Kazuma').sum()
 0
 >>> (df['name'] == 'Kaz').sum()
 1
 >>> df[df['name'] == 'Kaz']
-    region sex  year name  count      freq
+        region sex  year name  count      freq
 2424961     LA   M  2015  Kaz      5  0.000002
+"""
+(df['name'] == 'Kazuma').sum()
+0
+(df['name'] == 'Kaz').sum()
+1
+df[df['name'] == 'Kaz']
+#         region sex  year name  count      freq
+# 2424961     LA   M  2015  Kaz      5  0.000002
+
+
+"""
+>>> (df['name'].str.startswith('Kaz')).sum()
+151
+>>> iskaz = df['name'].str.startswith('Kaz')
+>>> df[iskaz].sort_values('freq')
+        region sex  year      name  count      freq
+4225691     NY   M  2006      Kazi      5  0.000001
+4225692     NY   M  2006    Kazuki      5  0.000001
+562866      CA   F  2005  Kazandra      5  0.000001
+4223685     NY   M  2005      Kazi      5  0.000001
+5360426     TX   F  2000  Kazandra      5  0.000001
+...        ...  ..   ...       ...    ...       ...
+625232      CA   M  1918     Kazuo     22  0.000011
+626674      CA   M  1921     Kazuo     25  0.000012
+623394      CA   M  1913     Kazuo     13  0.000013
+627194      CA   M  1922     Kazuo     28  0.000013
+399285      CA   F  1927    Kazuko     31  0.000014
+[151 rows x 6 columns]
+"""
+
+
+"""
 >>> model.coef_
 array([[-5.56584447e-01, 5.14287250e-02, 2.69276390e-06, ...,
         -1.39283459e-04, -1.21600561e-04, -9.11513939e-05]])
->>> pd.Series(model.coef_, index=vectorizer.get_feature_names())
->>> pd.Series(model.coef_, index=vectorizer.get_feature_names()[0])
->>> index = vectorizer.get_feature_names()
->>> index
-['a',
- 'aa',
- 'aab',
-...
->>> index.shape
->>> type(index)
-list
->>> pd.Series(model.coef_[0], index=vectorizer.get_feature_names())
-a - 0.556584
-aa     0.051429
-aab    0.000003
-aac    0.021992
-aad    0.000415
-    ...
-zze - 0.000103
-zzi - 0.001919
-zzl - 0.000139
-zzm - 0.000122
-zzy - 0.000091
-Length: 5986, dtype: float64
->>> coef = pd.Series(model.coef_[0], index=vectorizer.get_feature_names())
->>> coef['Kaz']
->>> coef['az']
-- 0.02216603297465453
->>> coef['azu']
-- 0.0003656728319935028
->>> coef['zu']
-- 0.0016125466401391323
->>> coef['zum']
-5.621315934940933e-06
->>> coef['uma']
-0.0007642577222082215
->>> coef['ma']
-- 0.2804809937613128
->>> model.intercept_
-array([0.08277136])
->>> coef['z']
-- 0.06687313431457634
->>> coef['u']
-0.02968187895127858
->>> coef['m']
-- 0.03181555148151521
->>> coef['a']
-- 0.556584447334144
->>> coef['Ka']
->>> coef['K']
->>> coef.index
-Index(['a', 'aa', 'aab', 'aac', 'aad', 'aaf', 'aah', 'aai', 'aaj', 'aak',
+>>> coef = pd.Series(model.coef_[0], index=feature_names)
+>>> coef
+A     -0.143835
+Aa     0.033721
+Aab    0.000001
+Aad    0.000214
+Aah    0.000008
+         ...
+zze   -0.000098
+zzi   -0.001747
+zzl   -0.000139
+zzm   -0.000120
+zzy   -0.000094
+Length: 8624, dtype: float64
+>>> coef.sort_values().round(2)
+a     -0.50
+i     -0.34
+Mar   -0.30
+y     -0.27
+Ma    -0.25
        ...
-       'zyq', 'zyr', 'zys', 'zz', 'zza', 'zze', 'zzi', 'zzl', 'zzm', 'zzy'],
-      dtype='object', length=5986)
->>> coef['k']
-0.06634704179666573
->>> coef['ka']
-- 0.18216581846670435
->>> coef['kaz']
-3.0355019959739112e-05
->>> kazvec = vectorizer.transform('Kazuma')
+mes    0.27
+cha    0.27
+J      0.28
+am     0.32
+o      0.40
+"""
+coef = pd.Series(model.coef_[0], index=feature_names)
+print(coef.sort_values().round(2))
+# a     -0.50
+# i     -0.34
+# Mar   -0.30
+# y     -0.27
+# Ma    -0.25
+#        ...
+# mes    0.27
+# cha    0.27
+# J      0.28
+# am     0.32
+# o      0.40
+
+
+"""
 >>> kazvec = vectorizer.transform(['Kazuma'])
->>> coef[kazvec != 0]
->>> kazvec[kazvec != 0]
-matrix([[0.10064136, 0.24160243, 0.36640437, 0.11589853, 0.1563424,
-         0.40920386, 0.09844366, 0.12768782, 0.12058102, 0.26053805,
-         0.32475817, 0.17116995, 0.31639993, 0.49863121]])
->>> coef[kazvec != 0]
->>> kazvec[kazvec != 0]
-matrix([[0.10064136, 0.24160243, 0.36640437, 0.11589853, 0.1563424,
-         0.40920386, 0.09844366, 0.12768782, 0.12058102, 0.26053805,
-         0.32475817, 0.17116995, 0.31639993, 0.49863121]])
->>> kazvec[kazvec != 0].reshape()
->>> kazvec[kazvec != 0].reshape(kazvec.shape[1])
->>> kazvec[kazvec != 0].reshape((kazvec.shape[1],))
->>> kazvec[kazvec != 0].reshape((14,))
-matrix([[0.10064136, 0.24160243, 0.36640437, 0.11589853, 0.1563424,
-         0.40920386, 0.09844366, 0.12768782, 0.12058102, 0.26053805,
-         0.32475817, 0.17116995, 0.31639993, 0.49863121]])
->>> kazvec[kazvec != 0].flatten()
-matrix([[0.10064136, 0.24160243, 0.36640437, 0.11589853, 0.1563424,
-         0.40920386, 0.09844366, 0.12768782, 0.12058102, 0.26053805,
-         0.32475817, 0.17116995, 0.31639993, 0.49863121]])
->>> kazvec[kazvec != 0].ravel()
-matrix([[0.10064136, 0.24160243, 0.36640437, 0.11589853, 0.1563424,
-         0.40920386, 0.09844366, 0.12768782, 0.12058102, 0.26053805,
-         0.32475817, 0.17116995, 0.31639993, 0.49863121]])
->>> kazvec[kazvec != 0].astype(np.array)
->>> np.array(kazvec[kazvec != 0])
-array([[0.10064136, 0.24160243, 0.36640437, 0.11589853, 0.1563424,
-        0.40920386, 0.09844366, 0.12768782, 0.12058102, 0.26053805,
-        0.32475817, 0.17116995, 0.31639993, 0.49863121]])
+>>> kazvec
+<1x8624 sparse matrix of type '<class 'numpy.float64'>'
+    with 14 stored elements in Compressed Sparse Row format>
+>>> kazvec = pd.Series(kazvec.toarray()[0], index=feature_names)
+>>> kazvec = kazvec[kazvec.abs() > 0]
+>>> kazvec
+K      0.126702
+Ka     0.154755
+Kaz    0.390671
+a      0.100628
+az     0.235593
+azu    0.398854
+m      0.115413
+ma     0.159887
+u      0.114679
+um     0.248047
+uma    0.312667
+z      0.175519
+zu     0.341750
+zum    0.473007
+"""
+kazvec = vectorizer.transform(['Kazuma'])
+print(kazvec)
+# <1x8624 sparse matrix of type '<class 'numpy.float64'>'
+#     with 14 stored elements in Compressed Sparse Row format>
+pd.Series(kazvec.toarray()[0], index=feature_names)
+kazvec = pd.Series(kazvec.toarray()[0], index=feature_names)
+kazvec = kazvec[kazvec.abs() > 0]
+kazvec
+# K      0.126702
+# Ka     0.154755
+# Kaz    0.390671
+# a      0.100628
+# az     0.235593
+# azu    0.398854
+# m      0.115413
+# ma     0.159887
+# u      0.114679
+# um     0.248047
+# uma    0.312667
+# z      0.175519
+# zu     0.341750
+# zum    0.473007
+
+"""
 >>> np.array(kazvec[kazvec != 0])[0]
 array([0.10064136, 0.24160243, 0.36640437, 0.11589853, 0.1563424,
        0.40920386, 0.09844366, 0.12768782, 0.12058102, 0.26053805,
