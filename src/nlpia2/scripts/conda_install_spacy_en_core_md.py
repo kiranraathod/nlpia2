317a6b3e07eed4a126b3bc78e56eae7ba7aaf970
hobs <hobs@totalgood.com>
2022-04-09 14:12:29 -0700

    mv py scripts to within nlpia2.scripts module/package

--- src/nlpia2/scripts/conda_install_spacy_en_core_md.py
+++ src/nlpia2/scripts/conda_install_spacy_en_core_md.py
@@ -10,13 +10,14 @@ except (NameError, ImportError) as e:
     print(f'Downloading the **{name}** spacy language model...')
     spacy.cli.download(name)
     nlp = spacy.load(name)
+
 modeldir = Path(nlp._path).parent.parent
 files = os.listdir(str(modeldir))
 # print('\n'.join(files));
 name = 'en_core_web_md'
 if any(f.startswith(name) for f in files):
-    print(f'SpaCy {name} is already installed.')
+    print(f'SpaCy {name} is installed here: {modeldir}/{name}*')
 else:
     print(f"Couldn't find {name}\n  in {modeldir}\n  among {files}")
-    print(f'Downloading the **{name}** spacy language model...')
+    print(f'Trying again to download **{name}** spacy language model...')
     spacy.cli.download(name)
