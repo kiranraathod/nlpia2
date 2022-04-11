326b5eddca175bdcb263477a0f2aaa88d53d5892
hobs <hobs@totalgood.com>
2022-04-10 21:19:54 -0700

    correct one render path

--- src/nlpia2/scripts/render_html.sh
+++ src/nlpia2/scripts/render_html.sh
@@ -2,5 +2,5 @@
 
 python code/ch02/book_thief_sentence_split_graphviz.py
 python code/ch01/NLU_NLG_graphviz.py
-python code/render_html.py
+python code/scripts/render_html.py
 firefox $(pwd)/manuscript/html
