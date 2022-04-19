c839f3fb1b9a8edebfa35abcde03cd80349fcd30
hobs <hobs@totalgood.com>
2022-04-18 20:14:18 -0700

    crawling.py reverted to working version that can be canceled and will retain pages in dict in ram

--- src/nlpia2/ch05/crawling.py
+++ src/nlpia2/ch05/crawling.py
@@ -27,24 +27,21 @@ def walk_wikipedia(pages, depth=1, delay=0.1):
     depth = 0
     if isinstance(pages, str):
         pages = {t: None for t in pages.split(',')}
-    if not isinstance(pages, abc.Mapping):
+    elif not isinstance(pages, abc.Mapping):
         pages = {t: None for t in pages.split(',')}
-    queue = list(pages.keys())
-    while depth < depth_goal and len(queue):
-        log.info(f"depth={depth}, len(nextqueue)={len(queue)}, queue[0]={queue[0]}")
-        pages_this_level = len(queue)
-        for i in tqdm(range(pages_this_level)):
-            title = queue.pop(0)
-            page_tuple = pages.get(title)
-            # if title from front of queue has not already been retrieved, then retrieve it
-            if page_tuple is None:  # if False then retrieval has been attempted and failed
+    pages = pages
+    next_level = {t: None for t in pages}
+    while depth < depth_goal and len(next_level):
+        log.info(f"depth={depth}, len(nextlevel)={len(next_level)}, nextlevel[0]={list(next_level)[0]}")
+        thislevel = next_level
+        next_level = {}
+        for title in tqdm(thislevel):
+            if pages.get(title) is None:  # if False then retrieval has been attempted and failed
                 time.sleep(delay)
                 page = get_page(title)
                 log.debug(page)
-                pages[title] = (depth, i, page)
-                if page:  # page is False when get_page() failed to find a valid page
-                    queue.extend(list(page.links))
-            elif page_tuple and page_tuple[-1]:
-                queue.extend(list(page_tuple[-1].links))
+                pages[title] = (page, depth)
+                if page:  # page will sometimes be false when get_page() failed to find a valid page
+                    next_level.update({t: None for t in page.links if t not in pages})
         depth += 1
     return pages
