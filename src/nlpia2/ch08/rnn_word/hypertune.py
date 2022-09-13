ebb4c498faea8847cdacfe82a9998242db1664bb
hobs <hobs@totalgood.com>
2022-09-12 23:06:54 -0700

    no execfile

--- src/nlpia2/ch08/rnn_word/hypertune.py
+++ src/nlpia2/ch08/rnn_word/hypertune.py
@@ -15,10 +15,10 @@ def grid_search(
         kwargs['filename'] = (
             'model_epochs_{epochs}_rnn_type_{rnn_type}_nhid_{nhid}_batch_size_{batch_size}'
             '_bptt_{bptt}_nlayers_{nlayers}').format(**kwargs)
-        print(
-            ("python main.py {'--cuda' if cuda else ''} --epochs {epochs} --model_type {model_type}"
-             " --nhid {nhid} --batch_size {batch_size} --bptt {bptt} --nlayers {nlayers} --save {filename}.pt").format(**kwargs)
-        )
+        # print(
+        #     ("python main.py {'--cuda' if cuda else ''} --epochs {epochs} --model_type {model_type}"
+        #      " --nhid {nhid} --batch_size {batch_size} --bptt {bptt} --nlayers {nlayers} --save {filename}.pt").format(**kwargs)
+        # )
         print(kwargs)
         results = main(**kwargs)
         experiments.append(results)
