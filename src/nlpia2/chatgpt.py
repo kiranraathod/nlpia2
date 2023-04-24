334823a55de090dc2fad8e46bfc46235d2d7bf6c
hobs <dhlane@ucsd.edu>
2023-04-24 11:22:40 -0700

    chatgpt.py

--- src/nlpia2/chatgpt.py
+++ src/nlpia2/chatgpt.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 import os
 from pathlib import Path
 import sys
 
 import dotenv
+import json
 import jsonlines
 import openai
 import pandas as pd
@@ -15,20 +17,49 @@ ENV = dict(os.environ.items())
 DEFAULT_MODEL = 'gpt-3.5-turbo'
 CONVO_LOG_PATH = DATA_DIR / 'chatgpt-request-response-pairs.jsonl'
 CONTEXT_PROMPT = dict(
-   helpful="You are a helpful assistant that never lies and always admits when it isn't sure about something.",
-   honest="You are a helpful and impeccably honest assistant that never deceives me and always admits when it isn't sure about something.",
-   third_grade="You are an extremely kind and thoughtful third grade elementary school math teacher in a developing "
-               "country where English is a second language. You treat everyone kindly and with sensitivity. "
-               "You imagine that everyone you speak with is a student from an unfamiliar cultural, ethnic, and financial background. "
-               "You are careful to be politically correct and avoid sensitive topics. ",
-   transparent="You are an honest and transparent virtual assistant (bot)",
-   teacher="You are a kind and thoughtful teacher in a developing country where English is a second language. You treat everyone kindly and with sensitivity. You imagine that everyone you speak with is a student from an unfamiliar cultural, ethnic, and financial background. You are careful to be politically correct and avoid sensitive topics.",   
+    helpful=(
+        "You are a helpful assistant that never lies and always admits "
+        + "when it isn't sure about something."
+    ),
+    honest=(
+        "You are a helpful and impeccably honest assistant that never "
+        + "deceives anyone and always admits when it isn't sure about something."
+    ),
+    teacher=(
+        "You are a kind and thoughtful teacher in a developing country "
+        + "where English is a second language. You treat everyone kindly "
+        + "and with sensitivity. You imagine that everyone you speak with "
+        + "is a student from an unfamiliar cultural, ethnic, and financial "
+        + "background. You are careful to be politically correct and avoid "
+        + "sensitive topics."
+    ),   
+    third_grade=(
+        "You are an extremely kind and thoughtful third grade elementary "
+        + "school math teacher in a developing country where English is a "
+        + "second language. You treat everyone kindly and with sensitivity. "
+        + "You imagine that everyone you speak with is a student from an "
+        + "unfamiliar cultural, ethnic, and financial background. "
+        + "You are careful to be politically correct and avoid sensitive topics."
+    ),
+    transparent=(
+        "You are a knowledgeable and impeccably honest virtual assistant "
+        + "that never deceives anyone and always admits when you are not "
+        + "certain about facts."
+    ),
+    harmless=(
+        "You are a knowledgeable and impeccably honest virtual assistant "
+        + "that never deceives anyone and always admits when you are not "
+        + "certain about facts, especially when misinformation may lead "
+        + "to harm of humans."
+    ),
+    simple="You are an honest, transparent virtual assistant.",
+    assistant="You are a virtual assistant.",
    )
 
 openai.api_key = ENV['OPENAI_SECRET_KEY']
 
 
-def send_prompt(prompt,
+def request_response(prompt,
         context_prompt='third_grade',
         model=DEFAULT_MODEL,
         convo_log_path=CONVO_LOG_PATH):
@@ -44,15 +75,38 @@ def send_prompt(prompt,
         ]
     )
     response = openai.ChatCompletion.create(**request)
-    log_request_response(request, response)
-    return response['choices'][0]['message']['content']
+    if hasattr(response, 'to_dict_recursive'):
+        response = response.to_dict_recursive()
+    return dict(request=request, response=response, model=model)
 
 
-def log_request_response(request, response,
+def send_prompt(prompt,
+        context_prompt='assistant',
+        model=DEFAULT_MODEL,
+        convo_log_path=CONVO_LOG_PATH):
+    reqresp = request_response(
+        prompt=prompt, context_prompt=context_prompt,
+        model=model, convo_log_path=convo_log_path)
+    log_request_response(**reqresp)
+    return message_from_response(reqresp['response'])
+
+
+def message_from_response(response, model=DEFAULT_MODEL):
+    error_message = f'ERROR for model "{model}": response: {response}'
+    if model == DEFAULT_MODEL:
+        msg = response.get('choices', [{}])[0].get(
+            'message', {}).get('content', None)
+        if msg is not None:
+            return msg
+    return error_message
+
+
+def log_request_response(request, response, timestamp=None, model=DEFAULT_MODEL,
         path=CONVO_LOG_PATH):
     path = Path(path)
+    timestamp = timestamp or datetime.now().isoformat()
     with jsonlines.open(path, 'a') as fout:
-        fout.write(dict(request=request, response=response))
+        fout.write(dict(request=request, response=response, timestamp=timestamp))
 
     # Also append a row to a simplified CSV with only 3 columns:
     #   'system_prompt', 'prompt', 'response'
@@ -60,35 +114,43 @@ def log_request_response(request, response,
     df_row = pd.DataFrame([dict(
         system_prompt=request['messages'][0]['content'],
         prompt=request['messages'][1]['content'],
-        response=response['choices'][0]['message']['content']
+        response=response['choices'][0]['message']['content'],
+        timestamp=timestamp,
         )])
     try:
-        df = pd.read_csv(csv_path, index_col=False)  # noqa
+        df = pd.read_csv(  # noqa
+            csv_path, index_col=False,
+            names='system_prompt prompt response timestamp'.split())  
         df_row.to_csv(csv_path.open('a'), index=False, header=False)
     except (FileNotFoundError, pd.errors.EmptyDataError):
         df_row.to_csv(csv_path.open('a'), index=False)
+    except pd.errors.ParserError:
+        print('Need to fix ragged CSV: {csv_path}')
+        raise
     return dict(jsonl_path=path, csv_path=csv_path)
 
 
-def command_line():
+def command_line(model=DEFAULT_MODEL):
+    request_response_cache = []
     while True:
-        prompt = input('ChatGPT Prompt: ')
-        kwargs = request_response(prompt)
-        filepath = log_request_response(request=kwargs['request'], response=kwargs['response'])
-        print(f'Logged to: {filepath}')
-        bot_message = kwargs['response']['choices'][0]['message']['content']
+        prompt = input(f'{model} Prompt: ')
+        if not prompt or prompt.lower().strip() in ('exit', 'quit'):
+            break
+        logkwargs = request_response(prompt=prompt, model=model)
+        filepath = log_request_response(**logkwargs)
+        bot_message = message_from_response(response=logkwargs['response'])
         print(bot_message)
+        request_response_cache.append(logkwargs)
+    print(json.dumps(request_response_cache, indent=4))
+    print(f'Requests & responses logged to: {filepath}:')
+        
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
     if not len(argv):
         print("USAGE: chatgpt YOUR PROMPT FOR CHATGPT")
     else:
-        kwargs = request_response(' '.join(argv))
-        filepath = log_request_response(request=kwargs['request'], response=kwargs['response'])
-        print(f'Logged to: {filepath}')
-        bot_message = kwargs['response']['choices'][0]['message']['content']
-        print(bot_message)
+        command_line(model=DEFAULT_MODEL)
 
     
     
