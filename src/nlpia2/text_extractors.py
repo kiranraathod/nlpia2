a8d8b25974220df815b25d3e7d10b594e404d325
hobs <dhlane@ucsd.edu>
2023-04-28 14:17:36 -0700

    sfx

--- src/nlpia2/text_extractors.py
+++ src/nlpia2/text_extractors.py
@@ -11,6 +11,8 @@ from types import MappingProxyType
 from nlpia2.text_processing.re_patterns import RE_URL_WITH_SCHEME, RE_URL_SIMPLE  # noqa
 from nlpia2.constants import SRC_DATA_DIR, MANUSCRIPT_DIR
 
+print("IMPORTING TEXT_EXTRACTORS")
+
 RE_TEXT_LINE = r"^[A-Z_\*][-A-Za-z\ 0-9 :\";',!@#$%^&*()_+-={}<>?,.\/]+"
 RE_TITLE_LINE = r"^[=]+[A-Za-z0-9\ \-?!,]+"
 RE_MARKUP_LINE = r"^[\[][A-Za-z0-9,\ ]+[\]]"
@@ -69,24 +71,33 @@ def extract_lines(text=DEFAULT_FILEPATH, with_meta=True):
 def extract_code_lines(filepath=DEFAULT_FILEPATH, with_metadata=True, section_break=None):
     """ Extract lines of Python using DocTestParser, return list of strs """
     text = Path(filepath).open('rt').read()
+    print('len(text):')
     print(len(text))
     sections = extract_expression_sections(text=text, section_break=section_break)
+    print('sections:')
     print(sections)
     # assert len(sections) > 0
     if not isinstance(sections, list):
         sections = [sections]
 
     if with_metadata:
+        print('with_metadata')
+        print(len(sections))
         return [[vars(expr) for expr in sect] for sect in sections] 
-    return [expr.source for expr in sections[0]]
+
+    print('WITHOUT_metadata')
+    return [[expr for expr in sect] for sect in sections]
 
 
 def extract_expression_sections(text, section_break='// SECTIONBREAK'):
     """ Use doctest.DocTestParser to find lines of Python code in doctest format """
+    print('text:')
+    print(text)
     dtparser = DocTestParser()
     
     if not section_break:
-        return dtparser.get_examples(text)
+        print('NO SECT_BREAK')
+        return [dtparser.get_examples(text)]
 
     text_sections = ['']
     for line in text.splitlines():
@@ -334,10 +345,19 @@ def extract_code_file(filepath=DEFAULT_FILEPATH, destfile=None):
     destfile = Path(destfile) if destfile else filepath.with_suffix('.adoc.py')
     if destfile.is_dir():
         destfile = destfile / filepath.with_suffix('.adoc.py').name
-    lines = extract_code_lines(filepath=filepath, with_metadata=False)
+    sections_lines = extract_code_lines(filepath=filepath, with_metadata=True)
+    if sections_lines:
+        print('sections_lines[0]')
+        print(sections_lines[0])
+        
     if destfile:
-        with Path(destfile).open('wt') as fout:
-            fout.writelines(lines)
+        destfile = Path(destfile)
+        if sections_lines:
+            for i, lines in enumerate(sections_lines): 
+                sfx = f'.sect{i:02d}{destfile.suffix}'
+                with destfile.with_suffix(sfx).open('wt') as fout:
+                    fout.writelines('\n'.join(lines))
+        
     return ''.join(lines)
 
 
