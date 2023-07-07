fabd9cb87226943280f34c531d76d948a3f73c0f
hobs <dhlane@ucsd.edu>
2023-07-07 09:45:15 -0700

    refresh_manuscript

---
+++ scripts/refresh_manuscript.sh
@@ -0,0 +1,4 @@
+# rm -f src/nlpia2/data/manuscript/adoc/Chapter*.adoc
+ln -f ../../nlpia-manuscript/manuscript/adoc/Chapter*.adoc src/nlpia2/data/manuscript/adoc/
+# rm -f src/nlpia2/data/manuscript/adoc/Chapter*.adoc
+ln -f ../../nlpia-manuscript/manuscript/adoc/Appendix*.adoc src/nlpia2/data/manuscript/adoc/
