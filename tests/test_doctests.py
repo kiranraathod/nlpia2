5b615cb151eb98f482b429bb33dd278dd1636800
hobs <dhlane@ucsd.edu>
2023-07-11 16:56:48 -0700

    disable doctests

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -12,13 +12,13 @@ from pathlib import Path
 # ADOC_DIR = Path(__file__).parent.parent.parent / 'nlpia-manuscript' / 'manuscript' / 'adoc'
 from nlpia2.constants import ADOC_DIR
 
-CH2_PATH = Path(list(ADOC_DIR.glob('Chapter-02*'))[0])
+CH2_PATH = Path(list(ADOC_DIR.glob('Chapter-02_*'))[0])
 
 __author__ = "Hobson Lane"
 __copyright__ = "Hobson Lane"
 __license__ = "mit"
 
 
-def test_manuscript_ch2():
-    results = test_file(CH2_PATH, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
-    assert results.failed == 0
+# def test_manuscript_ch2():
+#     results = test_file(CH2_PATH, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+#     assert results.failed == 0
--- tests/test_manuscript.py
+++ tests/test_manuscript.py
@@ -12,12 +12,13 @@ __license__ = "mit"
 
 
 def test_chapter(ch=2):
-    f""" Test Chapter {ch} """
-    filepath = list(ADOC_DIR.glob(f'Chapter {ch:02d} -- *'))[0]
+    f""" Test Chapter-{ch:02d}_* """
+    filepath = list(ADOC_DIR.glob(f'Chapter-{ch:02d}_*'))[0]
     results = test_file(filepath, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
     assert results.failed == 0
 
 
-def test_all(chapters=range(1, 13)):
+def test_chapters(chapters=range(1, 13)):
+    f""" Test Chapters {list(chapters)[0]:02d}-{list(chapters)[-1]:02d} """
     for ch in chapters:
         test_chapter(ch=ch)
