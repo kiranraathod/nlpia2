2e33fa16002c14b75bc1be45ceab66ea5428ca05
hobs <dhlane@ucsd.edu>
2023-07-11 17:00:44 -0700

    donttest_manuscript

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -1,24 +1,15 @@
-# -*- coding: utf-8 -*-
-
 import pytest  # noqa
 import doctest
 import nlpia2
 import nlpia2.ch06
-import nlpia2.ch06.spell  # noqa
-from nlpia2.text_processing.extractors import test_file
-
-from pathlib import Path
-
-# ADOC_DIR = Path(__file__).parent.parent.parent / 'nlpia-manuscript' / 'manuscript' / 'adoc'
-from nlpia2.constants import ADOC_DIR
-
-CH2_PATH = Path(list(ADOC_DIR.glob('Chapter-02_*'))[0])
-
-__author__ = "Hobson Lane"
-__copyright__ = "Hobson Lane"
-__license__ = "mit"
+# import nlpia2.ch06.spell
 
 
-# def test_manuscript_ch2():
-#     results = test_file(CH2_PATH, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
-#     assert results.failed == 0
+def test_ch06_spell_doctests():
+    results = doctest.testmod(nlpia2.ch06.spell, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+    assert results.failed == 0
+
+
+def test_nlpia2_doctests():
+    results = doctest.testmod(nlpia2, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+    assert results.failed == 0
