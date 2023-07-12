f04f34a5e649b8641c87bdd0ef9e11b960b94c58
hobs <dhlane@ucsd.edu>
2023-07-11 17:08:47 -0700

    no ch06.spell

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -5,9 +5,9 @@ import nlpia2.ch06
 # import nlpia2.ch06.spell
 
 
-def test_ch06_spell_doctests():
-    results = doctest.testmod(nlpia2.ch06.spell, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
-    assert results.failed == 0
+# def test_ch06_spell_doctests():
+#     results = doctest.testmod(nlpia2.ch06.spell, optionflags=(doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+#     assert results.failed == 0
 
 
 def test_nlpia2_doctests():
