128a6e43bc915bf57dc033007a5765481dca2e9d
hobs <dhlane@ucsd.edu>
2023-07-13 16:39:31 -0700

    refresh manuscript

--- scripts/refresh_manuscript.sh
+++ scripts/refresh_manuscript.sh
@@ -7,19 +7,19 @@
 
 # rm -f src/nlpia2/data/manuscript/adoc/Chapter*.adoc
 # git commit -am 'rm Chapter*'
-ln -f ../../nlpia-manuscript/manuscript/adoc/Chapter*.adoc src/nlpia2/data/manuscript/adoc/
+ln -f ../nlpia-manuscript/manuscript/adoc/Chapter*.adoc src/nlpia2/data/manuscript/adoc/
 # git commit -am 'hard link Chapter*'
 
 # rm -f src/nlpia2/data/manuscript/adoc/Appendix*.adoc
 # git commit -am 'rm Appendix*'
-ln -f ../../nlpia-manuscript/manuscript/adoc/Appendix*.adoc src/nlpia2/data/manuscript/adoc/
+ln -f ../nlpia-manuscript/manuscript/adoc/Appendix*.adoc src/nlpia2/data/manuscript/adoc/
 # git commit -am 'hard link Appendix*'
 
-ln -f ../../nlpia-manuscript/manuscript/adoc/TOC* src/nlpia2/data/manuscript/adoc/
-ln -f ../../nlpia-manuscript/manuscript/adoc/outline* src/nlpia2/data/manuscript/adoc/
+ln -f ../nlpia-manuscript/manuscript/adoc/TOC* src/nlpia2/data/manuscript/adoc/
+ln -f ../nlpia-manuscript/manuscript/adoc/outline* src/nlpia2/data/manuscript/adoc/
 
 # rm -f src/nlpia2/data/manuscript/adoc/Glossary.adoc
 # git commit -am 'rm Glossary.adoc'
-ln -f ../../nlpia-manuscript/manuscript/adoc/Glossary.adoc src/nlpia2/data/manuscript/adoc/
+ln -f ../nlpia-manuscript/manuscript/adoc/Glossary.adoc src/nlpia2/data/manuscript/adoc/
 # git commit -am 'hard link Glossary.adoc'
 
