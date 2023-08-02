a62e1cdf1fbc1143950c30f939a0d3c956bd8cef
hobs <dhlane@ucsd.edu>
2023-08-02 12:32:11 -0700

    windows install instructions

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,14 +6,15 @@
 # source .venv/bin/activate
 
 if [ -z `which aptitude` ] ; then
-    echo "We don't appear to be on an Ubuntu machine, so you'll need to install your binaries."
+    echo "You don't appear to be on an Ubuntu machine, so you'll need to install your binaries."
 else
     sudo aptitude install libpython3-dev build-essential git automake xdotool autoconf intltool autopoint
     sudo apt install asciidoctor
 fi
-conda create -n nlpia2 install -y --upgrade 'python==3.8.8' pip
+conda create -n nlpia2 install -y --upgrade 'python==3.11.4' pip
 conda activate nlpia2
-conda install jupyter pandas scikit-learn scipy nltk spacy -c pytorch pytorch torchtext -c spacy spacy-lookups-data
+conda install jupyter pandas scikit-learn scipy nltk spacy -c pytorch pytorch torchtext -c spacy spacy-lookups-data graphviz dot
+conda install -c conda-forge git-bash
 # pip install -y -r requirements.txt
 python -m spacy download en_core_web_md
 # pip install spacy-lookups-data  # for lemmatization
