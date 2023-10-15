7d4f65a0769020db856620c6380cc20839236a2a
hobs <dhlane@ucsd.edu>
2023-10-15 11:14:33 -0700

    notebook extractor is working!

--- src/nlpia2/text_processing/converters.py
+++ src/nlpia2/text_processing/converters.py
@@ -21,12 +21,17 @@ Typical DocTestParser Expression objects:
  'exc_msg': None}
 """
 from doctest import DocTestParser
+import logging
 from pathlib import Path
 import re
 import nbformat as nbf
+from tqdm import tqdm
+
 from nlpia2.text_processing.extractors import parse_args
 from nbformat.v4 import new_notebook, new_markdown_cell, new_code_cell
 import sys
+
+log = logging.getLogger(__name__)
 HEADER_TEXT = f"""\
 # Imports and Settings
 
@@ -69,7 +74,7 @@ CODEHEADER = [
 ]
 
 
-def re_matches(patterns, lines, match_objects=False):
+def re_matches(patterns, lines, match_objects=True):
     r""" Match multiple regular expressions to consecutive lines of text
 
     >>> patterns = '^([.][\\w\\s]+)?\s*$\n^\[source\]$\n^[-]+$'.split('\n')
@@ -114,15 +119,17 @@ def get_code_blocks(text,
         if not len(header_matches) >= min_header_len:
             i += 1
             continue
-        line_number = i - 1 * bool((header_matches or [''])[0].strip())
+        line_number = i
+        if header_matches[0].group().strip():
+            line_number -= 1
         i += len(header_matches)
         # last line of header must match
-        if not re.match(header_patterns[-1], header_matches[-1]):
+        if not re.match(header_patterns[-1], header_matches[-1].group()):
             i += 1
             continue
         block = []
         # TODO: include docutils examples(parsed code and output examples)
-        while i < len(lines):
+        while i < len(lines) - min_footer_len:
             block.append(lines[i])
             i += 1
             footer_matches = re_matches(footer_patterns, lines[i:])
@@ -133,16 +140,16 @@ def get_code_blocks(text,
             preceding_text=lines[line_number - 1],
             preceding_blank_line=lines[line_number],
             line_number=line_number,
-            header='\n'.join(header_matches),
+            header='\n'.join([m.group() for m in header_matches]),
             code='\n'.join(block),
-            footer='\n'.join(footer_matches),
+            footer='\n'.join([m.group() for m in footer_matches]),
             following_blank_line=lines[i],
             following_text=lines[i + 1],
         ))
     return blocks
 
 
-def adoc_doctests2ipynb(adocs=None, dest_filepath=None, **kwargs):
+def adoc_doctests2ipynb(adocs=Path('.'), dest_filepath=None, **kwargs):
     adocs = Path(adocs)
     text = kwargs.pop('text', None) or ''
     if adocs.is_file():
@@ -170,17 +177,32 @@ def adoc_doctests2ipynb(adocs=None, dest_filepath=None, **kwargs):
     return nb
 
 
-def adoc2ipynb(adocs=None, dest_filepath=None, **kwargs):
-    adocs = Path(adocs)
-    text = kwargs.pop('text', None) or ''
-    if adocs.is_file():
-        text = text + '\n' + adocs.read()
+def find_title(text, pattern=r'^\s*[=#]\s?(.+)$'):
+    r""" First first line that matches pattern (r'^\s*[=#]\s?.+$')"""
+    for line in text.splitlines():
+        if re.match(pattern, line):
+            return line
+    for line in text.splitlines():
+        if line.strip():
+            return line
+
+
+def adoc2ipynb(filepath=None, dest_filepath=None, text=None):
+    try:
+        text = Path(filepath).open().read()
+    except (TypeError, OSError, IOError, FileNotFoundError) as e:
+        log.error(e)
+
     dest_filepath = dest_filepath if not dest_filepath else Path(dest_filepath)
     blocks = get_code_blocks(text)
 
     nb = new_notebook()
     cells = []
-    cells.append(new_markdown_cell(f"#### {adocs}"))
+    title = find_title(text)
+    if filepath:
+        title = f'[{filepath.with_suffix("").name}]({filepath})'
+    if filepath:
+        cells.append(new_markdown_cell(f"#### _`{title}`_"))
 
     for block in blocks:
         # need to run the doctest parser on a lot of text to get attr names right
@@ -193,6 +215,17 @@ def adoc2ipynb(adocs=None, dest_filepath=None, **kwargs):
         with dest_filepath.open('w') as f:
             nbf.write(nb, f)
     return nb
+
+
+def adocs2notebooks(adoc_dir=Path('.'), dest_dir=None, glob='*.adoc'):
+    """ Convert a directory of adoc files into jupyter notebooks """
+    notebooks = []
+    for p in tqdm(list(adoc_dir.glob(glob))):
+        dest_filepath = dest_dir
+        if dest_filepath:
+            dest_filepath /= p.with_suffix('.adoc.ipynb').name
+        notebooks.append(adoc2ipynb(text=p.open().read(), dest_filepath=dest_filepath))
+    return notebooks
 
 
 def convert(format='ipynb', **kwargs):
