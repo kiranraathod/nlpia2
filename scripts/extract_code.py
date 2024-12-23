d78b67b3758ae65c6283eca870d7c5d71a96987f
hobs <github@totalgood.com>
2024-12-22 19:30:24 -0800

    undefined adoc2outline fun

--- scripts/extract_code.py
+++ scripts/extract_code.py
@@ -7,11 +7,12 @@ from nlpia2.text_processing.converters import adocs2notebooks
 # from nlpia2.constants import BASE_DIR as NLPIA2_BASE_DIR
 
 try:
+    # tangibleai/nlpia2/
     BASE_DIR = Path(__file__).parent.parent.parent
 except Exception:
     BASE_DIR = Path.cwd()
 assert BASE_DIR.is_dir(), "not {BASE_DIR}.is_dir()"
-assert BASE_DIR.name == 'nlpia2'
+assert BASE_DIR.name == 'nlpia'
 
 ADOC_DIR = BASE_DIR / 'manuscript' / 'adoc'
 assert ADOC_DIR.is_dir(), "not {ADOC_DIR}.is_dir()"
@@ -20,7 +21,9 @@ if __name__ == '__main__':
     adoc_dir = ADOC_DIR
     if sys.argv[1:]:
         adoc_dir = Path(sys.argv[1])
-    dest_dir = BASE_DIR / 'notebooks'
-    print(adoc_dir, '->', dest_dir)
-    nbs = adocs2notebooks(adoc_dir, dest_dir=dest_dir, glob='Chapter-*.ipynb')
     dest_dir = BASE_DIR / 'code' / 'notebooks'
+    print(adoc_dir, '->', dest_dir)
+    nbs = adocs2notebooks(adoc_dir=adoc_dir, dest_dir=dest_dir, glob='CH*.adoc')
+    print(nbs)
+    # nbs = adocs2notebooks(adoc_dir, dest_dir=BASE_DIR / 'src' / 'nlpia2' / 'notebooks', glob='CH*.adoc')
+    print(nbs)
